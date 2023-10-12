c4c7de7c3e42d29cee11b936aac79f413d19f67a
a198rrav <140890166+a198rrav@users.noreply.github.com>
2023-10-12 16:50:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,73 @@
+//https://teachablemachine.withgoogle.com/models/NukT2nDws/
+
+Webcam.set({
+    width:350,
+    height:301,
+    image_format:'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>'
+    })
+}
+console.log('ml5 version:',ml5.version);
+classifier =ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/NukT2nDws/model.json',modelloaded );
+
+function modelloaded(){
+    console.log('Model Loaded!');
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1="The first prediction is "+prediction_1;
+    speak_data_2="And the second prediction is "+prediction_2;
+    var utterThis =new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+    synth.speak(utterThis);
+}
+
+
+function check() {
+    img=document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,result){
+    if(error){
+        console.error(error);
+    }    else{
+        console.log(result);
+        document.getElementById("result_emotion_name").innerHTML =result[0].label;
+        document.getElementById("result_emotion_name2").innerHTML =result[1].label;
+        prediction_1 =result[0].label;
+        prediction_2 =result[1].label;
+        speak();
+        if(result[0].label =="I love you"){
+            document.getElementById("update_emoji_1").innerHTML = "&#129311;";
+        }
+    }
+    if(result[0].label =="folded hands"){
+        document.getElementById("update_emoji_1").innerHTML = "&#128400;";
+    }
+    
+    if(result[0].label =="ok"){
+        document.getElementById("update_emoji_1").innerHTML = "&#&#128079";
+
+        if(result[1].label =="folded hands"){
+            document.getElementById("update_emoji_2").innerHTML = "&#128400;";
+        }
+
+        if(result[1].label =="ok"){
+            document.getElementById("update_emoji_2").innerHTML = "&#128079;";
+        }
+
+        if(result[1].label =="I love you"){
+            document.getElementById("update_emoji_2").innerHTML = "&#129311;";
+        }
+    }
+}
